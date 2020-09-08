6a8a59723e06481e85e9b84dcda41061ce8398a8
Developer Raj <rajdeepkakar@gmail.com>
2020-09-07 20:59:00 -0400

    Creating an outline of the common layout structure that can be used be used for each page. So far, defined two layouts: 1.) Coding Quiz Home Page 2.) Questionnaire-Page layout.

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,3 @@
-// # 04 Web APIs: Code Quiz
-
-// As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
-
-// ## User Story
-
-// ```
-// AS A coding bootcamp student
-// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-// SO THAT I can gauge my progress compared to my peers
-// ```
-
-// ## Acceptance Criteria
-
-// ```
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
@@ -24,19 +9,36 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and score
-// ```
 
-// The following animation demonstrates the application functionality:
+// Global Variables
 
-// ![code quiz](./Assets/04-web-apis-homework-demo.gif)
+//List out the distinct screen layouts in order and identify the element(s) that will be used in each row
+//Layout_1: Coding Quiz Challenge
+//-----nav:<nav-brand></nav-brand> & <timer></timer>
+//-----row-1:<h1></h1>
+//-----row-2:<p1></p1>
+//-----row-3:<button class="btn btn-start-quiz">Start Quiz</button>
 
-// ### Review
+//Layout_2: Questionnaire-page (5)
+//-----nav:<nav-brand></nav-brand> & <timer></timer>
+//-----row-1:
+//---------col-sm-12
+//---------------<h2>Questions Go Here</h2>
+//-----row-2:
+//---------col-sm-6
+//--------------row-1-q-1
+//--------------row-1-q-1
+//--------------row-1-q-1
+//--------------row-1-q-1
+//----------------<ol>
+//-------------------<button class="btn btn-answer-options"></button>
+//-------------------<button class="btn btn-answer-options"></button>
+//-------------------<button class="btn btn-answer-options"></button>
+//-------------------<button class="btn btn-answer-options"></button>
+//---------------<ol>
+//---------col-sm-6
+//-----row-2:
+//---------col-sm-12
+//---------------<p>Wrong! or Correct!<p>
 
-// You are required to submit the following for review:
-
-// * The URL of the functional, deployed application.
-
-// * The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-// - - -
-// © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+//Functions
